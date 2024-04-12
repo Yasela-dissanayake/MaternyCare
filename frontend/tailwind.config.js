733c2abf6417cb126f1f51991ff22f9ca59469bf
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-12 13:19:22 +0530

    feat: frontend initialized

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Look for classes in all JavaScript/TypeScript files within the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
