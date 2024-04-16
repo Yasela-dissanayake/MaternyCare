a6588f762ff31fa9c1346d2bfa47bda7d8184217
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-16 19:35:49 +0530

    feat: drawer button state changes when clicked

--- frontend/src/assets/icons/Icons.ts
+++ frontend/src/assets/icons/Icons.ts
@@ -2,10 +2,12 @@ import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
 import BookOnlineIcon from "@mui/icons-material/BookOnline";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export {
   SpaceDashboardIcon as DashboardIcon,
   BookOnlineIcon as AppointmentsIcon,
   EventNoteIcon as NoticesIcon,
   ManageAccountsIcon as ProfileIcon,
+  LogoutIcon as LogoutIcon,
 };
