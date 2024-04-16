f091e226b8f9961df708c41478c1daefb72c067b
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-16 17:51:57 +0530

    feat: drawer links added

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
+import {
+  AppointmentsIcon,
+  DashboardIcon,
+  NoticesIcon,
+  ProfileIcon,
+} from "../assets/icons/Icons";
+const navLinks = [
+  {
+    name: "Dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    name: "Notices",
+    icon: <NoticesIcon />,
+  },
+  {
+    name: "Appointments",
+    icon: <AppointmentsIcon />,
+  },
+  {
+    name: "Profile",
+    icon: <ProfileIcon />,
+  },
+];
 
 const Drawer = () => {
-  return <div>drawer</div>;
+  return (
+    <div className="px-10 py-12 flex flex-col border border-r-1 w-80 h-screen">
+      <div className="logo-div flex space-x-3 items-center">
+        <img src={logo} className=""></img>
+        <span>Materny Care</span>
+      </div>
+
+      <div className="mt-10 flex flex-col space-y-8">
+        {navLinks.map((item, index) => (
+          <div key={index} className="flex items-center space-x-2">
+            {item.icon}
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Drawer;
