f5f836ae9a599973220e94b2ea96819a5d5e889e
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-16 17:51:57 +0530

    feat: drawer links added

---
+++ frontend/src/assets/icons/Icons.ts
@@ -0,0 +1,11 @@
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import BookOnlineIcon from "@mui/icons-material/BookOnline";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+
+export {
+  SpaceDashboardIcon as DashboardIcon,
+  BookOnlineIcon as AppointmentsIcon,
+  EventNoteIcon as NoticesIcon,
+  ManageAccountsIcon as ProfileIcon,
+};
