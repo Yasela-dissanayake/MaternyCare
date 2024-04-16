7e5574b179eab840df2a1a078a5742dd687f6922
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-16 19:02:28 +0530

    style: drawer button active state style created

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import logo from "../assets/images/logo.png";
 import {
   AppointmentsIcon,
@@ -6,6 +5,7 @@ import {
   NoticesIcon,
   ProfileIcon,
 } from "../assets/icons/Icons";
+import { useState } from "react";
 const navLinks = [
   {
     name: "Dashboard",
@@ -26,6 +26,8 @@ const navLinks = [
 ];
 
 const Drawer = () => {
+  const [activeLink, setActiveLink] = useState(0);
+
   return (
     <div className="px-10 py-12 flex flex-col border border-r-1 w-80 h-screen">
       <div className="logo-div flex space-x-3 items-center">
@@ -35,9 +37,14 @@ const Drawer = () => {
 
       <div className="mt-10 flex flex-col space-y-8">
         {navLinks.map((item, index) => (
-          <div key={index} className="flex items-center space-x-2">
+          <div
+            key={index}
+            className={`flex items-center space-x-2 py-3 px-3 rounded-xl	 ${
+              activeLink == index ? "text-[#0D99FF] bg-[#CAE9FF]" : ""
+            }`}
+          >
             {item.icon}
-            <span>{item.name}</span>
+            <span className="pl-1">{item.name}</span>
           </div>
         ))}
       </div>
