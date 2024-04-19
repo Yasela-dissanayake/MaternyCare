a8e3e9e33f5883cd568c466b1e05bb8affa836ba
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-19 16:56:32 +0530

    feat: notification bell icon added with badge

--- frontend/src/components/RightBar.tsx
+++ frontend/src/components/RightBar.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
-const RightBar = () => {
-  return <div>RightBar</div>;
+const Drawer: React.FC = () => {
+  const [activeLink, setActiveLink] = useState<number>(0);
+
+  return (
+    <div className="px-10 py-12 flex flex-col border border-r-0 w-80 h-screen ml-auto">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <IconButton
+            aria-label="show new notifications"
+            color="inherit"
+            sx={{ padding: 1, width: "auto" }}
+          >
+            <Badge badgeContent={5} color="error">
+              <NotificationsNoneIcon />
+            </Badge>
+          </IconButton>
+        </div>
+        <div>a</div>
+        <div>a</div>
+      </div>
+    </div>
+  );
 };
 
-export default RightBar;
+export default Drawer;
