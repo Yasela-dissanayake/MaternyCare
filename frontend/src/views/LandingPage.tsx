ca60cf45fc6c138fcf5b2f074dafb4e39d3744b8
Methmina Binara <binaraepa@gmail.com>
2024-04-22 00:48:11 +0530

    feat:Inserted logo into landing page

--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
 
 const LandingPage = () => {
-  return <div className="text-center text-4xl">LandingPage</div>;
+  return (
+    <div>
+    
+    {/* <div className="flex justify-center items-start h-screen">
+      <div className="justify-center mt-16">
+        <img src={logo} alt="Materny Logo" className="w-24" />
+      </div>
+    </div>
+
+    <div>
+        <h1 style={{ color: "#F580AB", fontSize: "2.25rem", fontFamily: "Ubuntu" }}>Materny Care</h1>
+    </div> */}
+  
+
+  <div className="flex flex-col justify-center items-center h-screen">
+      <div className="justify-center mt-16">
+        <img src={logo} alt="Materny Logo" className="w-24" />
+      </div>
+      <div className="mt-4 text-center">
+        <h1 style={{ color: "#F580AB", fontSize: "2.25rem", fontFamily: "Ubuntu" }}>Materny Care</h1>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+  
+    </div>
+  );
 };
 
 export default LandingPage;
