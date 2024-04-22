adb2845d09560be29b498b246aaaef5d2f80f69e
Sahan-Maleesha <smaleesha84@gmail.com>
2024-04-22 17:21:38 +0530

    Style: componet width change to 40%

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="text-center text-4xl">Login</div>;
+  return <div className="text-center text-4xl"><img src={""} alt="" /></div>;
 };
 
 export default Login;
