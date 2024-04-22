78761933d7ff3ceff20ef4096e8984f702ae83e9
Sahan-Maleesha <smaleesha84@gmail.com>
2024-04-22 17:21:38 +0530

    Style: componet width change to 40%

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -12,7 +12,7 @@ const InputField: React.FC<InputFieldProps> = ({ value, onChange }) => {
 
   return (
     <input
-      className={`shadow appearance-none border rounded-b-xl py-4 px-4 w-auto text-gray-700 leading-tight focus:outline-[#0D99FF] focus:shadow-outline`}
+      className={`shadow appearance-none border rounded-b-xl py-4 px-4 w-5/12	 text-gray-700 leading-tight focus:outline-[#0D99FF] focus:shadow-outline`}
       id={value}
       type="text"
       //   value={value}
