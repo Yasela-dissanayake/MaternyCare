f5da6644cb078bcb0042ed4d1b32ee64a834174f
Sahan-Maleesha <smaleesha84@gmail.com>
2024-04-22 18:09:23 +0530

    Style: Change primary button width and hover color

--- frontend/src/components/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton.tsx
@@ -10,7 +10,7 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({ onClick, label }) => {
   return (
     <button
       onClick={onClick}
-      className={`text-xl px-44 sm:px-60 py-5 rounded-xl text-white  ${bgStyles.primaryBlue} ${bgStyles.secondaryBlueHover}`}
+      className={`text-xl py-5 rounded-xl text-white w-5/12 h-16  ${bgStyles.primaryBlue} ${bgStyles.loginHover}`}
     >
       {label}
     </button>
