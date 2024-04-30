14cfddb826ca255102fb522ccea099a7a5e5a5b9
Yasela-dissanayake <yasela2014@gmail.com>
2024-04-30 09:38:42 +0530

    style: dashboard bannar color changed

--- frontend/src/views/Dashboard.tsx
+++ frontend/src/views/Dashboard.tsx
@@ -21,9 +21,9 @@ const Dashboard = () => {
         </div>
       </div>
       <h1 className="mt-9 mb-4">Dashboard Overview</h1>
-      <div className="h-44 px-8 py-5 text-white bg-[#F580AB] rounded-2xl mb-8">
+      <div className="h-44 px-8 py-5 text-white bg-[#BA97FE] rounded-2xl mb-8">
         <h1 className="mb-2">
-          Hello, <span className="font-semibold">Ushani,</span>
+          Hello <span className="font-semibold">Ushani,</span>
         </h1>
         <p>
           Have a nice day and don’t forget to take care of <br />
@@ -32,17 +32,15 @@ const Dashboard = () => {
         <h5 className="mt-3">Learn More </h5>
       </div>
       <div className="grid grid-cols-3 gap-5">
-        <div className="bg-red-100 pt-9 pl-7 h-36 rounded-lg grid grid-cols-2">
+        <div className="bg-white pt-9 pl-7 h-36 rounded-lg grid grid-cols-2">
+          <div>{/* <img src={logo} alt="" className="h-12" /> */}</div>
           <div>
-            <img src={logo} alt="" className="h-12" />
-          </div>
-          <div>
-            <h1>5 kicks</h1>
-            <h1>Kick count</h1>
+            {/* <h1>5 kicks</h1>
+            <h1>Kick count</h1> */}
           </div>
         </div>
-        <div className="bg-red-100 pt-9 pl-7 h-36 rounded-lg">a</div>
-        <div className="bg-red-100 pt-9 pl-7 h-36 rounded-lg">a</div>
+        <div className="bg-white pt-9 pl-7 h-36 rounded-lg"></div>
+        <div className="bg-white pt-9 pl-7 h-36 rounded-lg"></div>
       </div>
     </div>
   );
--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import MUIResponsiveDrawer from "../components/MUIResponsiveDrawer";
+import RightBar from "../components/RightBar";
 
 const LandingPage = () => {
   return <div className="text-center text-4xl">LandingPage</div>;
