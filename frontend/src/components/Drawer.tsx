e73d2b365cb31f402d46e6d410896b8ffaea4b9f
Methmina Binara <binaraepa@gmail.com>
2024-04-30 10:10:41 +0530

    Work in progress: Creating carousel

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import logo from "../assets/images/logo.png";
+import { navLinks } from "../data/Data";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useMatch } from "react-router-dom";
 
 const Drawer = () => {
-  return <div>drawer</div>;
+  return (
+    <div className="px-10 py-12 flex flex-col bg-white rounded-r-2xl w-1/5">
+      <div className="flex flex-col items-center justify-center">
+        <img src={logo} className="h-36"></img>
+        <h1 className="text-[#F580AB] text-2xl font-medium	">Materny Care</h1>
+      </div>
+
+      <div className="mt-10 flex flex-col space-y-8 text-[#666666] ">
+        {navLinks.map((item, index) => {
+          const match = useMatch(item.path);
+
+          return (
+            <NavLink to={item.path} key={index}>
+              <button
+                className={`flex items-center space-x-2 py-3 px-3 rounded-xl w-full ${
+                  match ? "text-[#0D99FF] bg-[#CAE9FF]" : ""
+                }`}
+              >
+                <item.icon />
+                <span className="pl-1 ">{item.name}</span>
+              </button>
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Drawer;
