d2192df0607535b0b1b45723e64bbbcdef4c0051
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-02 14:16:13 +0530

    feat: redesigned drawer with headless UI disclosure component

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -50,7 +50,11 @@ const Drawer = () => {
      */}
 
       <Disclosure as="nav">
+<<<<<<< HEAD
         <Disclosure.Button className="absolute top-5 right-10 cursor-pointer peer inline-flex focus:outline-none lg:hidden">
+=======
+        <Disclosure.Button className="absolute top-5 right-4 cursor-pointer peer inline-flex focus:outline-none lg:hidden">
+>>>>>>> 40c0594e (feat: redesigned drawer with headless UI disclosure component)
           <MenuIcon />
         </Disclosure.Button>
 
@@ -58,6 +62,12 @@ const Drawer = () => {
           // className={`px-10 py-12 bg-white rounded-r-2xl w-[300px]  fixed top-0 bottom-0 overflow-y-auto  `}
           className=" px-10 py-12 w-[300px] h-screen bg-white z-20 fixed top-0 -left-[300px] lg:w-70 lg:left-0 peer-focus:left-0 peer:transition ease-out delay-150 duration-200"
         >
+<<<<<<< HEAD
+=======
+          {/* <div className="lg:hidden flex justify-end">
+            <CloseIcon className="cursor-pointer" />
+          </div> */}
+>>>>>>> 40c0594e (feat: redesigned drawer with headless UI disclosure component)
           <div className="flex flex-col items-center justify-center">
             <img src={logo} className="h-36"></img>
             <h1 className="text-[#F580AB] text-2xl font-medium	">
