faf5c56d9c7fc35496571c91d006597e8d496198
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-05 18:02:37 +0530

    feat: created dbinit folder for future deployment purpose

--- backend/src/routes/notice.routes.ts
+++ backend/src/routes/notice.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { getNotice, getNotices } from "../controller/notice.controller";
 
 const noticeRoutes = Router();
+
+noticeRoutes.route("/").get(getNotices);
+
+noticeRoutes.route("/:noticeId").get(getNotice);
 
 export default noticeRoutes;
