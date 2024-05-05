d622e70f6430b16aff7ca4a4b74f0e467dd0e59b
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-05 16:05:58 +0530

    feat:added getNotice function the contriller

--- backend/src/query/notice.query.ts
+++ backend/src/query/notice.query.ts
@@ -1,18 +1,9 @@
 export const QUERY = {
-<<<<<<< HEAD
-    SELECT_NOTICES: 'SELECT * FROM notices ORDER BY created_at DESC LIMIT 50',
-    SELECT_NOTICE: 'SELECT * FROM notices WHERE id = ?',
-    CREATE_NOTICE: 'INSERT INTO notices (first_name, last_name, email, address, diagnosis, phone, status, image_url) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
-    UPDATE_NOTICE: 'UPDATE notices SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, status = ?, image_url = ? WHERE id = ?',
-    DELETE_NOTICE: 'DELETE FROM notices WHERE id = ?'
-};
-=======
   SELECT_NOTICES: "SELECT * FROM notices ORDER BY created_at DESC LIMIT 50",
   SELECT_NOTICE: "SELECT * FROM notices WHERE id = ?",
   CREATE_NOTICE:
-    "INSERT INTO notices (title, sub_title, message, image_url) VALUES(?, ?, ?, ?)",
+    "INSERT INTO notices (first_name, last_name, email, address, diagnosis, phone, status, image_url) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
   UPDATE_NOTICE:
-    "UPDATE notices SET title = ?, sub_title = ?, message = ?, image_url = ?",
+    "UPDATE notices SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, status = ?, image_url = ? WHERE id = ?",
   DELETE_NOTICE: "DELETE FROM notices WHERE id = ?",
 };
->>>>>>> 70e8ba9b (WIP)
