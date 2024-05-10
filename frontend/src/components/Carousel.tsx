a1e121cfd37a5c2e76302646db375c9bf32e964f
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-10 19:44:08 +0530

    WIP

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -1,59 +1,58 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
-import './carousel.module.css';
+import "./carousel.module.css";
 
-import Mother from '../assets/images/mother.png';
-import PHM from '../assets/images/phm.png';
-import MOH from '../assets/images/moh.png';
+import Mother from "../assets/images/mother.png";
+import PHM from "../assets/images/phm.png";
+import MOH from "../assets/images/moh.png";
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 export default function Carousel() {
   return (
     <>
-    <div className="carouselContainer">
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={3}
-        coverflowEffect={{
-          rotate: 20,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide className="slide ">
-          <div className='border-pink-500 border-2 rounded-3xl bg-[#FBD0E0]'>
-            <img className='my-10' src={Mother} alt="Mother" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="slide">
-        <div className='border-pink-500 border-2 rounded-3xl bg-[#FBD0E0]'>
-            <img className='my-10' src={PHM} alt="Mother" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="slide">
-        <div className='border-pink-500 border-2 rounded-3xl bg-[#FBD0E0]'>
-            <img className='my-10' src={MOH} alt="Mother" />
-          </div>
-        </SwiperSlide>
-        
-      </Swiper>
-    </div>
+      <div className="carouselContainer">
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 20,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="slide ">
+            <div className="border-pink-500 border-2 rounded-3xl bg-[#FBD0E0]">
+              <img className="my-10" src={Mother} alt="Mother" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="slide">
+            <div className="border-pink-500 border-2 rounded-3xl bg-[#FBD0E0]">
+              <img className="my-10" src={PHM} alt="Mother" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="slide">
+            <div className="border-pink-500 border-2 rounded-3xl bg-[#FBD0E0]">
+              <img className="my-10" src={MOH} alt="Mother" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 }
--- frontend/src/views/Registration.tsx
+++ frontend/src/views/Registration.tsx
@@ -5,21 +5,20 @@ import RegistrationPage_1 from "../components/RegistrationPage_1";
 const Registration = () => {
   return (
     <div className="flex flex-col justify-start items-center h-screen bg-[#F5F5F5]">
-        <div className="mt-10">
-          <img src={logo} alt="Materny Logo" className="w-24" />
-        </div>
+      <div className="mt-10">
+        <img src={logo} alt="Materny Logo" className="w-24" />
+      </div>
 
-        <div className="mt-0 text-center leading-9 font-semibold">
-          <h1 style={{ fontFamily: "Ubuntu", fontSize: "2.25rem" }}>
-            <span style={{ color: "#0D99FF" }}>Materny</span>
-            <span style={{ color: "#F580AB" }}> Care</span>
-          </h1>
-        </div>
-    
-        <div>
-          <RegistrationPage_1 />
-        </div>
+      <div className="mt-0 text-center leading-9 font-semibold">
+        <h1 style={{ fontFamily: "Ubuntu", fontSize: "2.25rem" }}>
+          <span style={{ color: "#0D99FF" }}>Materny</span>
+          <span style={{ color: "#F580AB" }}> Care</span>
+        </h1>
+      </div>
 
+      <div>
+        <RegistrationPage_1 />
+      </div>
     </div>
   );
 };
