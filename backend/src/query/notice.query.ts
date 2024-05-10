32de29f99f50578429932f5742a3ce3c79b9a438
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-10 13:49:53 +0530

    feat:notice query update

--- backend/src/query/notice.query.ts
+++ backend/src/query/notice.query.ts
@@ -2,8 +2,8 @@ export const QUERY = {
   SELECT_NOTICES: "SELECT * FROM notices ORDER BY created_at DESC LIMIT 50",
   SELECT_NOTICE: "SELECT * FROM notices WHERE id = ?",
   CREATE_NOTICE:
-    "INSERT INTO notices (first_name, last_name, email, address, diagnosis, phone, status, image_url) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+    "INSERT INTO notices (title, sub_title, message, image_url) VALUES(?, ?, ?, ?)",
   UPDATE_NOTICE:
-    "UPDATE notices SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, status = ?, image_url = ? WHERE id = ?",
+    "UPDATE notices SET title = ?, sub_title = ?, message = ?, image_url = ? WHERE id = ?",
   DELETE_NOTICE: "DELETE FROM notices WHERE id = ?",
 };
