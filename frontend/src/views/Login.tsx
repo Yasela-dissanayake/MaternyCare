9630dc1dfa7422174de18fdcbe615c798699f405
Sahan-Maleesha <smaleesha84@gmail.com>
2024-05-12 14:47:29 +0530

    WIP

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,22 +1,31 @@
-import logo  from "../assets/images/logo.png";
-import {textStyles} from "../GlobalStyles"
-import InputField from "../components/InputField";
-import  Button  from "../components/PrimaryButton";
+import {useFormik} from "formik";
 
 const Login = () => {
-  return <div className="text-center flex flex-col items-center">
-    <img className="w-[10%] h-[10%] mt-[96px] mb-[12px]" src={logo} alt="" />
-    <h1 className={`${textStyles.primaryPink} text-4xl mb-[48px]`}>MaternyCare</h1>
-    <InputField value="Name" onChange={() =>{}}/>
-    <div className="p-5"/>
-    <InputField value="Password" onChange={() =>{}}/>
-    <div className="p-3"/>
-    <div className="flex justify-start w-5/12">
-    <p className={`${textStyles.primaryGray} text-xs`}>Forgot Password?</p>
-    </div>
-    <div className="p-3"/>
-    <Button label="Login" onClick={() =>{}}/>
-  </div>;
+  const {values, handleBlur, handleChange} = useFormik({
+    initialValues: {
+      name: '',
+      password: ''
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  });
+  
+
+  return(
+    <form autoComplete="off">
+      <label htmlFor="name">Name</label>
+      <input
+      value={values.name}
+      onChange={handleChange} 
+      type="text"
+      placeholder="Name"
+      onBlur={handleBlur}
+       />
+    </form>
+  )
+
+  
 };
 
 export default Login;
